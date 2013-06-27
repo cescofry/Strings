bde7a374285810694c923ae2fdde0a1019082a37
Francesco Frison <cescofry@gmail.com>
2013-06-27 11:30:13 +0100

    add basic command line call. Parsing not implemnted yet

--- Strings/main.m
+++ Strings/main.m
@@ -15,17 +15,30 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
+        NSString *arg = (argc == 2)? [NSString stringWithUTF8String:argv[1]] : @"--help";
+        
+        if ([arg isEqualToString:@"--help"]) {
+            printf("Strings is a tool to convert iOS to Android translations file and vice versa.\nPlease use strings <ROOT URL> to start\n");
+            return 0;
+        }
+        
         // insert code here...
         NSLog(@"Starting Strings:");
-        NSString *urlString = [NSString stringWithUTF8String:argv[0]];
-        NSLog(@"Scanning %@", urlString);
+        
+        NSURL *url = [NSURL URLWithString:arg];
+        if (!url){
+            printf("The directory provided is not valid. Exiting\n");
+            return 0;
+        }
+        
+        printf("Scanning %s\n", url.absoluteString.UTF8String);
         
         
         
         ZFStringScanner *scanner = [[ZFStringScanner alloc] init];
-        [scanner scanStringsAtURL:[NSURL URLWithString:urlString]];
+        [scanner startScanAtURL:url];
         
-        NSLog(@"Done, exiting");
+        printf("Nothing Done. Exiting\n");
         
     }
     return 0;
